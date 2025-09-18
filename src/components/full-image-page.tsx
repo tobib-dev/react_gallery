936feccd1df7e35b049cafb3c2470d3e5742a5fd
tobii19-py <tobi19.py@gmail.com>
2025-09-18 09:25:22 -0500

    move to the grid

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -4,7 +4,7 @@ export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
   return (
     <div className="flex h-full w-full min-w-0">
-      <div className="flex-shrink flex justify-center items-center">
+      <div className="flex flex-shrink items-center justify-center">
         <img src={image.url} className="flex-shrink object-contain" />
       </div>
 
